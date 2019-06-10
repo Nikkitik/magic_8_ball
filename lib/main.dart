334c54d4f9c2d9f38a8cda94a8af11f294e86940
nikita.romantsov <nikkitik21@gmail.com>
2019-06-10 16:11:53 +0300

    реализовал выбор ответ по состоянию датчиков

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:aeyrium_sensor/aeyrium_sensor.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -37,37 +39,68 @@ class _BallState extends State<Ball> {
 
   String ballAnswer = '';
 
+  int lastValue = 0;
+  bool change = false;
+
+  StreamSubscription<dynamic> _streamSubscriptions;
+
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: FlatButton(
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: <Widget>[
-            Image.asset('images/ball.png'),
-            Text(
-              ballAnswer,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Fresca',
-                fontSize: 16.0,
-                color: Colors.blue.shade200,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-        onPressed: () {
-          getTextFromBall();
-        },
-      ),
-    );
+  void initState() {
+    _streamSubscriptions = AeyriumSensor.sensorEvents.listen((event) {
+      int value = ((event.pitch + event.roll) * 100).round();
+
+      if (lastValue == value && change) {
+        changeAnswerBall();
+        change = false;
+
+        print('lastValue == value && $change');
+      }
+
+      if ((lastValue - value).abs() > 50) {
+        lastValue = value;
+        change = true;
+
+        print('(lastValue - value).abs() > 50 && $change');
+      }
+    });
+
+    super.initState();
   }
 
-  void getTextFromBall() {
+  void changeAnswerBall() {
     setState(() {
       ballAnswer = '${ballMap[Random().nextInt(5)]}\n';
     });
+  }
+
+  @override
+  void dispose() {
+    if (_streamSubscriptions != null) {
+      _streamSubscriptions.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: <Widget>[
+          Image.asset('images/ball.png'),
+          Text(
+            ballAnswer,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Fresca',
+              fontSize: 16.0,
+              color: Colors.blue.shade200,
+              letterSpacing: 2.5,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
